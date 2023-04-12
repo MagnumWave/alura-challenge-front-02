8ae90232b75e63bd6ceb84a862a35fee06264c78
MagnumWave <aureliomagno@terra.com.br>
2023-04-12 20:52:34 -0300

    Dia 04: ajustes de layout e color picker funcionando

--- src/app/components/editor-de-codigo/editor-de-codigo.component.ts
+++ src/app/components/editor-de-codigo/editor-de-codigo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-editor-de-codigo',
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditorDeCodigoComponent implements OnInit {
 
+  @ViewChild('realColorPicker') realColorPicker: HTMLInputElement | undefined;
+  pickedColor = "#000000"
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  abreColorPicker(arg: HTMLInputElement){
+    // console.log(arg.value);
+
+      arg.click();
+      // this.isColorPickerOpened = true;
+
+
+
+    // this.realColorPicker.nativeElement;
+  }
+
+  handleChangeColorPicker(arg: HTMLInputElement){
+    console.log(arg.value);
+    this.pickedColor = arg.value;
+
+
   }
 
 }
--- src/shared/components/highlight-viewer/highlight-viewer.component.ts
+++ src/shared/components/highlight-viewer/highlight-viewer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-highlight-viewer',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./highlight-viewer.component.css']
 })
 export class HighlightViewerComponent implements OnInit {
+
+  @Input('color') color = '#000';
 
   constructor() { }
 
